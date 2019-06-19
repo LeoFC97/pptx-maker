6c41504d5121a288708f4c088c0f41c99b0cdb8b
LeoFC97 <leonardo.fc2@outlook.com>
2019-06-19 00:05:23 -0300

    feature: function for creating the master sliders

--- index.js
+++ index.js
@@ -8,8 +8,8 @@ const robots = {
 
 async function start() {
  // robots.input()
- // await robots.text()
-  //await robots.image()
+  //await robots.text()
+ // await robots.image()
   await robots.powerpoint()
 }
 
