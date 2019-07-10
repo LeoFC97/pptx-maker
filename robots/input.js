ad9dbf3733f7d029713b833d78141563732a55c4
Leonardo Costa <leonardo.fc2@outlook.com>
2019-07-10 18:30:43 -0300

    feature: 100% of the text is translated by the chosen lang

--- robots/input.js
+++ robots/input.js
@@ -9,57 +9,50 @@ class Robot{
       content.lang = this.askAndReturnLanguage();
       content.author = this.askAndReturnAuthor(content.lang);
       content.searchTerm = this.askAndReturnSearchTerm(content.lang);
-      content.font = this.askAndReturnFont() || 'Arial';
-      content.prefix = this.askAndReturnPrefix();
-      content.prefixLang = this.askAndReturnPrefixLang(content.prefix, content.lang);
+      content.font = this.askAndReturnFont(content.lang) || 'Arial';
+      content.prefix = this.askAndReturnPrefix(content.lang);
       state.save(content)
   }
-  selectTextByLanguage(lang, en, pt) {
+  selectTextByLanguage(lang, textInEnglish, textInPortuguease) {
     switch(lang) {
       case "pt":
-        return pt;
+        return textInPortuguease;
       default:
-        return en;
+        return textInEnglish;
     }
   }
   askAndReturnLanguage(){
     const language = ['pt','en']
-    const selectedLangIndex = readline.keyInSelect(language,'Escolha o Idioma / Choose Language: ')
+    const selectedLangIndex = readline.keyInSelect(language,'Choose Language/ Escolha o Idioma: ')
     const selectedLangText = language[selectedLangIndex]
     return selectedLangText
   }
   askAndReturnAuthor(lang) {
-    let question = this.selectTextByLanguage(lang, 'Type your name: ', 'Digite o seu nome: ');
+    const question = this.selectTextByLanguage(lang, 'Type your name: ', 'Digite o seu nome: ');
     return readline.question(question);
   }
-   askAndReturnSearchTerm(lang) { 
-      let question = this.selectTextByLanguage(lang, 'Type a Wikipedia search term: ','Digite o termo a ser pesquisado na Wikipedia: ');
+  askAndReturnSearchTerm(lang) { 
+      const question = this.selectTextByLanguage(lang, 'Type a Wikipedia search term: ','Digite o termo a ser pesquisado na Wikipedia: ');
       return readline.question(question)
     }
-   askAndReturnFont(){
-    return readline.question('Type a font [Default: Arial]: ')
+  askAndReturnFont(lang){
+    const question= this.selectTextByLanguage(lang,'Type a font [Default: Arial]: ', 'Digite uma fonte[Padrao: Arial]: ')
+    return readline.question(question)
   }
 
-   askAndReturnPrefix() {
-    const prefixes = ['Who is', 'What is', 'The history of']
-    const selectedPrefixIndex = readline.keyInSelect(prefixes, 'Choose one option: ')
+   askAndReturnPrefix(lang) {
+    let prefixes=[]
+    const question = this.selectTextByLanguage(lang,'Chose one option: ', 'Escolha uma opcao')
+    if(lang==='pt'){
+      prefixes = ['Quem e', 'O que e', 'A Historia de']
+    }
+    else if(lang==='en'){
+      prefixes = ['Who is', 'What is', 'The history of']
+    }
+    const selectedPrefixIndex = readline.keyInSelect(prefixes, question)
     const selectedPrefixText = prefixes[selectedPrefixIndex]
 
     return selectedPrefixText
-  }
-
-   askAndReturnPrefixLang(prefix, lang) {
-    if(lang === 'en')
-      return prefix
-
-    if(lang === 'pt') {
-      if(prefix === 'Who is')
-        return 'Quem é'
-      else if (prefix === 'What is')
-        return 'O Que é'
-      else
-        return 'A História'
-    }
   }
 }
 
