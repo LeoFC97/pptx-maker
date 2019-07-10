422385ce9c33d0f22ea9b439c38ca6df2fde2311
Leonardo Costa <leonardo.fc2@outlook.com>
2019-07-10 16:18:17 -0300

    feature: tests on lang change

--- __tests/input-robot.test.js
+++ __tests/input-robot.test.js
@@ -1,25 +1,23 @@
 const Robot = require('../robots/input');
 
 describe('Input Robot', () => {
-
-  it('Should ask and return Language PT', () => {
-    const language = ['pt','en']
-    const selectedLangIndex = 0;
-    const selectedLangText = language[selectedLangIndex]
-    expect(selectedLangText).toBe('pt');
+  it('text must be in english', () => {
+    const lang = "en";
+    const en = " Type your name: ";
+    const pt = "Digite o seu nome: ";
+    
+    const text = selectTextByLanguage(lang, en, pt);
+  
+    expect(text).toBe(en);
   });
-  it('Should ask and return Language EN', () => {
-    const language = ['pt','en']
-    const selectedLangIndex = 1;
-    const selectedLangText = language[selectedLangIndex]
-    expect(selectedLangText).toBe('en');
-  });
-  it('Should question Author name in PTBR', () =>{
-    Robot.setText('Digite o seu nome: ')
-    expect(Robot.getText()).toBe('Digite o seu nome: ');
-  });
-  it('Should question Author name in EN', () =>{
-    Robot.setText('Type your name: ')
-    expect(Robot.getText()).toBe('Type your name: ');
+  it('text must be in pt', () => {
+    const lang = "pt";
+    const en = " Type your name: ";
+    const pt = "Digite o seu nome: ";
+    
+    const text = selectTextByLanguage(lang, en, pt);
+  
+    expect(text).toBe(pt);
   });
 });
+
