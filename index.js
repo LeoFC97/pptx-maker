af507494266ac77d23df909b6fdc78e516bab39f
Rafael Teixeira <rafael.amtx@gmail.com>
2019-07-11 00:30:03 -0300

    front-end-electron

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+const { remote } = require("electron");
+
 const robots = {
   input: require('./robots/input.js'),
   text: require('./robots/text.js'),
@@ -5,6 +7,42 @@ const robots = {
   image: require('./robots/image.js'),
   powerpoint: require('./robots/powerpoint.js')
 }
+
+var inputs = {
+  lang: "",
+  name: "",
+  term: "",
+  font: "",
+  option: ""
+}
+
+function makeInputsAndPpt(){
+  inputs.lang = document.getElementById("selectLang").value
+  inputs.name = document.getElementById("inputName").value
+  inputs.term = document.getElementById("inputTerm").value
+  inputs.font = document.getElementById("inputFont").value
+  inputs.option = document.getElementById("inputOption").value
+  start()
+}
+
+/*
+Comentado até a implementação do front-end
+document.getElementById("btnDoPpt").addEventListener("click", () => {
+  makeInputsAndPpt();
+});
+
+document.getElementById("close").addEventListener("click", () => {
+  remote.BrowserWindow.getFocusedWindow().close();
+});
+
+document.getElementById("minimize").addEventListener("click", () => {
+  remote.BrowserWindow.getFocusedWindow().minimize();
+});
+
+document.getElementById("maximize").addEventListener("click", () => {
+  remote.BrowserWindow.getFocusedWindow().maximize();
+});
+*/
 
 async function start() {
   robots.input.start()
